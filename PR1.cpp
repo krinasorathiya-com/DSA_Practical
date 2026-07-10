9341cd763e52f42b0c93f5d6ad7130a6989b2457
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-07-10 13:19:40 +0530

    refactor main function to improve array handling and input prompts

--- PR1.cpp
+++ PR1.cpp
@@ -3,15 +3,27 @@ using namespace std;
 
 int main()
 {
-    int n,h,k;
-    cout<<"Enter the number of elements in the array: ";
-    cin>>n;
-    int arr[n];
-    cout<<"Enter Total Hour : ";
-    cin>>h;
-    
-    for(int i=0;i<k;i++)
+    int n, h;
 
+    cout << "Enter number of items: ";
+    cin >> n;
 
+    int arr[100];
 
+    cout << "Enter the items: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Enter number of hours: ";
+    cin >> h;
+
+    h = h % n;
+
+    cout << "Final display order: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[(i + h) % n] << " ";
+    }
+
+    return 0;
 }
