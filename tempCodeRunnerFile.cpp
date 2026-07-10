42904f996f790e24453336c1daf5f96e6b825812
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-07-10 13:20:09 +0530

    add initial implementation of main function with item input prompts

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n, h;
+
+    cout << "Enter number of items: ";
+    cin >> n;
+
+    int arr[100];
+
+    cout << "Enter the items: ";
