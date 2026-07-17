a605cbe48e7a13aeba0003647024796ac1b88c6a
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-07-17 13:24:35 +0530

    Implement basic string input functionality in PR1.3.cpp

--- PR1.3.cpp
+++ PR1.3.cpp
@@ -1,1 +1,27 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main()
+{
+    string sentence;
+    cout << "Enter a string: ";
+    getline(cin, sentence);
+
+    stringstream ss(sentence);
+    string word, longestWord;
+
+    while (ss >> word)
+    {
+        if (word.length() > longestWord.length())
+        {
+            longestWord = word;
+        }
+    }
+
+    cout << "Longest word: " << longestWord << endl;
+    cout << "Length: " << longestWord.length() << endl;
+
+    return 0;
+}
