4ca44fcb12c890057c466e3d518cc783e59e5a4b
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-08-07 13:31:06 +0530

    Fix variable naming and remove redundant size calculation in iterative linear search

--- Pr2.1.(Iterative).cpp
+++ Pr2.1.(Iterative).cpp
@@ -14,16 +14,13 @@ int iterativeLinearSearch(string plates[], int size, string target)
 int main()
 {
     string plates[] = { "JK10QS2108", "KN11RT0408", "MO13EF1234", "HK03HK6909"};
-    int size = sizeof(plates) / sizeof(plates[0]);
-
     string target;
     cout << "Enter the license plate to search: ";
     cin >> target;
 
-    int iterativeResult = iterativeLinearSearch(plates, size, target);
-    
-    if (iterativeResult != -1)
-        cout << "Iterative Search: Plate found at position " << iterativeResult << endl;
+    int Result = iterativeLinearSearch(plates, size, target);
+    if (Result != -1)
+        cout << "Iterative Search: Plate found at position " << Result << endl;
     else
         cout << "Iterative Search: Plate not found" << endl;
 
